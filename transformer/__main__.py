269071a34f9b437949d50bbb3a78f7c36714b04f
Aaron Schmid <aschmid@mysmartheart.ch>
2021-11-29 15:41:49 +0100

    Added messages when transformation ended

--- transformer/__main__.py
+++ transformer/__main__.py
@@ -7,12 +7,15 @@ def run_default_transformer(users_path, restaurants_path, ratings_path):
     restaurants_transformer = RestaurantsTransformer(restaurants_path, '.\\output\\restaurant_features.csv')
 
     users_transformer.transform()
+    print("Transformed user_features to .\\output\\user_features.csv")
     restaurants_transformer.transform()
+    print("Transformed user_features to .\\output\\restaurant_features.csv")
 
     ratings_transformer = RatingsTransformer(ratings_path, '.\\output\\ratings.csv',
         user_ids=users_transformer.ids, restaurant_ids=restaurants_transformer.ids)
     
     ratings_transformer.transform()
+    print("Transformed user_features to .\\output\\ratings.csv")
 
 if __name__ == '__main__':
     run_default_transformer(
